0b6a7cda1902591de8e6d4563a018ff45ec61101
hollieherbel <herbel6521@gmail.com>
2020-07-01 21:08:58 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from flask_heroku import Heroku
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+heroku = Heroku(app)
+CORS(app)
+
+
